3587f3284424ee4da184e3242169ea3688e286c4
supriyanaidu <32039542+supriyanaidu@users.noreply.github.com>
2018-04-04 15:51:09 -0600

    fixed the colorscale

--- viz1/viz1.js
+++ viz1/viz1.js
@@ -18,11 +18,15 @@ var margin = {top: 50, right: 0, bottom: 100, left: 120},
   colors = ["#48A36D", "#56AE7C", "#64B98C", "#72C39B", "#80CEAA", "#80CCB3", "#7FC9BD", "#7FC7C6", "#7EC4CF", "#7FBBCF", "#7FB1CF", "#80A8CE", "#809ECE", "#8897CE", "#8F90CD", "#9788CD", "#9E81CC", "#AA81C5", "#B681BE", "#C280B7", "#CE80B0", "#D3779F", "#D76D8F", "#DC647E", "#E05A6D", "#E16167", "#E26962", "#E2705C", "#E37756", "#E38457", "#E39158", "#E29D58", "#E2AA59", "#E0B15B", "#DFB95C", "#DDC05E", "#DBC75F", "#E3CF6D", "#EAD67C", "#F2DE8A"],
 	// colors1 = ["#1d91c0","#225ea8","#253494","#081d58"],
 	// colors2 = ["#edf8b1","#FFEE58","#F9A825","#FF9800", "#EF6C00"],
-	colors3 = ["#8897CE", "#9788CD","#AA81C5", "#CE80B0"],
+	// colors3 = ["#8897CE", "#9788CD","#AA81C5", "#CE80B0"],
 	colors4 = ["#deebf7", "#9ecae1","#7FBBCF", "#72C39B"],
 	colors5 = ["#E57373", "#EF5350","#C62828"],
-	colors1 = ["#b5cde1","#90b4d2","#6a9bc3","#4682b4", "#315b7d","#386890"],
-	colors2 = ["#edf8b1","#FFEE58","#F9A825","#FF9800", "#EF6C00"],
+	// colors1 = ["#b5cde1","#90b4d2","#6a9bc3","#4682b4", "#315b7d","#386890"],
+	colors1 = ["#f4c8c8","#eda3a3","#e47373","#e05d5d", "#dc4848","#d83333"],
+	colors2 = ["#fef0bc","#fee690","#fedc64","#fed74e","#fece22","#fec809"],
+	colors3 = ["#b2d9da", "#7fc1c2","#4ca8a9", "#198f91", "#008385", "#006a6c"],
+	colors4 = ["#c7d9e8","#a2c0d9","#7da7ca","#6a9bc3", "#588ebb","#4682b4"],
+	colors5 = ["#bfbfbf","#a6a6a6","#999999","#8c8c8c", "#808080","#737373"],
   dataset = "viz1.json";
 
 var svg = d3.select("#heatmap").append("svg")
@@ -66,7 +70,7 @@ var paramLabels = svg.selectAll(".paramLabel")
     return ((i >= 0 && i <= 9) ? "timeLabel mono axis axis-worktime" : "timeLabel mono axis");
   });
 
-svg.call(tip);
+svg.call(tip1);
 
 function heatmapChart(dataFile) {
   d3.json(dataFile,
@@ -139,7 +143,7 @@ function heatmapChart(dataFile) {
         .style("stroke", "white")
         .style('stroke-width', 0.3)
         .on('mouseover', function(d) {
-          tip.show(d);
+          tip1.show(d);
 
           d3.select(this)
             .style("opacity", 1)
@@ -147,7 +151,7 @@ function heatmapChart(dataFile) {
             .style("stroke-width", 3);
         })
         .on('mouseout', function(d) {
-          tip.hide(d);
+          tip1.hide(d);
 
           d3.select(this)
             .style("opacity", 0.8)
